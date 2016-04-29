f5bf141709c83ea4871c0f4f9be52846402d22f3
wjy <wjy@windows10.microdone.cn>
2016-04-29 17:06:35 +0800

    I modified the androidManifest.xml to add internet accessing permission

--- src/com/example/facedetection/MainActivity.java
+++ src/com/example/facedetection/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
 public class MainActivity extends Activity {
@@ -18,8 +21,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity1);
-
+        
+        Button btnButton1,btnButton2;
+        
+        btnButton1 = (Button) findViewById(R.id.btn1);
+        btnButton2 = (Button) findViewById(R.id.btn2);
+        
+        btnButton2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Log.i("TAG","motherfucker");
+				System.exit(0);
+				
+			}
+		});
+        
        
+      
     }
 
 
