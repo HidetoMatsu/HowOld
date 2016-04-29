d90d30c054ef5a5d9a9c1969d2d04a35e8e2b9d5
wjy <wjy@windows10.microdone.cn>
2016-04-29 17:28:19 +0800

    Added the constant.java

---
+++ src/com/example/facedetection/constant.java
@@ -0,0 +1,8 @@
+package com.example.facedetection;
+
+public class constant {
+	public static final String key="b0e6cc26640e43c0b7020aba8086dc4c";
+	public static final String secret="zu9NY7AlJKBDBLHt8A33B4FzSjCkXeOP";
+	
+
+}
