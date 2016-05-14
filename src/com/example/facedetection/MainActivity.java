6e7e67f6b91f2b0abbd6918c2c7a40bd355c4406
wjy <wjy@windows10.microdone.cn>
2016-05-14 20:34:14 +0800

    BuildAge bitmap function has been added

--- src/com/example/facedetection/MainActivity.java
+++ src/com/example/facedetection/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.facedetection;
 
+import com.example.menn.R;
+
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +16,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends Activity {
@@ -41,6 +45,25 @@ public class MainActivity extends Activity {
        
       
     }
+    
+
+	  private Bitmap buildAgeBitmap(int age, boolean isMale) {
+			// TODO Auto-generated method stub
+	    	TextView tv = (TextView) mWaiting.findViewById(R.id.id_age_gen);
+	    	tv.setText(age+"");
+	    	
+	    	if(isMale){
+	    		tv.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.drawable.male), null, null, null);
+	    	}
+	    	else{
+	    		tv.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.drawable.female),null, null, null);
+	    	}
+	    	tv.setDrawingCacheEnabled(true);
+	    	Bitmap bitmap = Bitmap.createBitmap(tv.getDrawingCache());
+	    	tv.destroyDrawingCache();
+			return bitmap;
+		}
+
 
 
  
